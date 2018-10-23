9f2f427d3ce2e50d2a11c1d2060272bef96eaf67
Graham Reeves <graham@grahamreeves.com>
2018-10-23 17:57:20 +0100

    fixed c# line intersections

--- Assets/LineIntersectionTest.cs
+++ Assets/LineIntersectionTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LineIntersectionTest : MonoBehaviour
 {
-
+	public bool ClampToLine = true;
 	public Transform StartA_Node;
 	public Transform EndA_Node;
 	public Transform StartB_Node;
@@ -22,57 +22,17 @@ public class LineIntersectionTest : MonoBehaviour
 	//	https://www.codefull.org/2015/06/intersection-of-a-ray-and-a-line-segment-in-3d/
 	void GetIntersection(out Vector3 IntersectionA, out Vector3 IntersectionB)
 	{
-		var segment_Start = StartB;
-		var segment_End = EndB;
-		var ray_Origin = StartA;
-		var ray_End = EndA;
-
-		//bool intersection(Ray ray, LineSegment segment)
-		Vector3 da = ray_End - ray_Origin;  // Unnormalized direction of the ray
-		Vector3 db = segment_End - segment_Start;
-
-		Vector3 dc = segment_Start - ray_Origin;
-		Vector3 dc2 = dc;//ray_Origin - segment_Start;
-
-		var dadb_cross = Vector3.Cross(da, db);
-		var dcdb_cross = Vector3.Cross(dc, db);
-		var dc2db_cross = Vector3.Cross(dc, da);
-		/*
-		var Dot = Vector3.Dot(dc, dadb_cross);
-		if (Mathf.Abs(Dot) >= coPlanerThreshold) // Lines are not coplanar
+		if (ClampToLine)
 		{
-			IntersectionA = Vector3.zero;
-			IntersectionB = Vector3.zero;
-			return;
+			PopMath.GetLineLineIntersection(StartA, EndA, StartB, EndB, out IntersectionA, out IntersectionB);
 		}
-*/
-		//	gr: total length sq?
-		float sa = Vector3.Dot(dcdb_cross,dadb_cross) / dadb_cross.sqrMagnitude;
-		float sb = Vector3.Dot(dc2db_cross,dadb_cross) / dadb_cross.sqrMagnitude;
-		/*
-		if (s < 0.0 || s > 1.0)
+		else
 		{
-			IntersectionA = Vector3.zero;
-			IntersectionB = Vector3.zero;
-			return;
-		}
-*/
-		IntersectionA = ray_Origin + (sa * da);
-		IntersectionB = segment_Start + (sb * db);
-		/*
-		// See if this lies on the segment
-		var segment_LengthSquared = Vector3.Distance(segment_Start, segment_End) * Vector3.Distance(segment_Start, segment_End);
-		//if ((intersection - segment_Start).sqrMagnitude + (intersection - segment_End).sqrMagnitude <= segment_LengthSquared + lengthErrorThreshold)
-		{
-			IntersectionA = intersection;
-			IntersectionB = intersection;
-			return;
+			var RayA = new Ray(StartA, EndA - StartA);
+			var RayB = new Ray(StartB, EndB - StartB);
+			PopMath.GetRayRayIntersection(RayA, RayB, out IntersectionA, out IntersectionB);
 		}
 
-		IntersectionA = Vector3.zero;
-		IntersectionB = Vector3.zero;
-		return;
-		*/
 	}
 	/*
 	void GetIntersection(out Vector3 IntersectionA,out Vector3 IntersectionB)
